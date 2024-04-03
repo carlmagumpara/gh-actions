8eddb89b5a58482884bf9286998b0ab15caf29e8
Carl Anthony Magumpara <carl@lexmeet.com>
2024-04-04 00:45:19 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import crying from './assets/tenor.gif'
+import audio from './assets/Male-Heavy-Whimpering-Crying-www.fesliyanstudios.com.mp3'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [result, setResult] = useState(null);
+  const [font_size, setFontSize] = useState(3.2);
+
+  const response = res => {
+    if (res === 'yes') {
+      setResult(res);
+    } else {
+      new Audio(audio).play();
+      setFontSize(prevState => prevState * 1);
+      setResult(res);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {!result ? <h1>Mag a outing ba tayo?</h1> : (
+        result === 'yes' ? <h1>Putangina tara</h1> : (
+          <>
+            <img src={crying} style={{ width: 200 }} />
+            <h1 style={{ fontSize: `${font_size}em` }}>Bakit hindi?</h1>
+          </>
+        )
+      )}
+      <div className="buttons">
+        <button onClick={() => response('yes')}>Yes</button>
+        <button onClick={() => response('no')}>No</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
